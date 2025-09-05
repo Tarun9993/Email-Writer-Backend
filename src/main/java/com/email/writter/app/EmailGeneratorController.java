1a763cdb74d555803ceb0eb0012938568afe65fb
Tarun <tarunkumartadala@gmail.com>
2025-09-05 21:05:46 +0530

    first commit

---
+++ src/main/java/com/email/writter/app/EmailGeneratorController.java
@@ -0,0 +1,20 @@
+package com.email.writter.app;
+
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/email")
+@AllArgsConstructor
+public class EmailGeneratorController {
+
+
+    private EmailGeneratorService emailGeneratorService;
+    @PostMapping("/generate")
+    public ResponseEntity<String> generateEmail(@RequestBody EmailRequest emailRequest){
+        String response = emailGeneratorService.generateEmailReply(emailRequest);
+        return ResponseEntity.ok(response);
+    }
+}
